a19ee9c46ff085b1dc08574837ccb9edec0cbb11
Jamshid K <jamshid.k@zeeve.io>
2023-02-12 10:26:16 +0530

    validation on serializer

--- watchlist_app/api/slzr.py
+++ watchlist_app/api/slzr.py
@@ -2,15 +2,29 @@ from rest_framework import serializers
 from watchlist_app.models import Watchlist
 
 
+def check_name(value):
+    if len(value) < 2:
+        raise serializers.ValidationError("Name is too short")
+    return value
+
+
 class WatchlistSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=100)
+    name = serializers.CharField(max_length=100,validators=[check_name])
     description = serializers.CharField(max_length=100)
     active = serializers.BooleanField()
 
     def create(self, validated_data):
         return Watchlist.objects.create(**validated_data)
 
+    def validate(self, validated_data):
+        if validated_data['name'] == validated_data['description']:
+            raise serializers.ValidationError("Name and description must not be same")
+        return validated_data
+    # def validate_name(self, value):
+    #     if len(value) <2:
+    #         raise serializers.ValidationError("Name is too short")
+    #     return value
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.description = validated_data.get('description', instance.description)
